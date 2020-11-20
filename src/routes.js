120efa7ce07f694d46f4d2c9e07afc8bcdbc52e2
georgetonjr <37754623+georgetonjr@users.noreply.github.com>
2020-11-20 18:38:56 -0300

    .

--- src/routes.js
+++ src/routes.js
@@ -31,8 +31,7 @@ const PrivateRoute = ({ component: Component, ...rest }) => (
 const Routes = () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path="/" component={()=> <h1>Página não encontrada</h1>} />
-      <Route exact path="/login" component={Login} />
+      <Route exact path="/" component={Login} />
       <Route path="/cadaluno" component={CadastroAluno} />
       <Route path="/cadprofessor" component={CadastroProfessor} />
       <PrivateRoute path="/app" component={() => <h1>App</h1>} />
